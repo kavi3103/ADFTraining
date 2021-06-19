3e096b7b6d9cf14bdc01ca0f3354e06f1dbad3f4
kavi3103 <48648214+kavi3103@users.noreply.github.com>
2021-06-19 13:47:43 +0530

    Add files via upload

---
+++ Day01/Program02/readCSVFile.py
@@ -0,0 +1,18 @@
+'''
+  2.	Program to read a CSV (CSV with n number of columns) and store it in DICT of list.
+'''
+
+import csv
+
+#inialise empty dictonary to store csv file rows
+dict = dict()
+count = 0
+
+#opens csv file Iris.csv in read mode
+with open("Iris.csv","r") as file:
+    reader = csv.reader(file)  #reads csv file rows
+    for row in reader:
+        dict[count] = row  #store rows in dictonary
+        count += 1
+
+print(dict)
