b34a4914872c43d99cfd5e1b4c07a114f5d915d1
kavi3103 <48648214+kavi3103@users.noreply.github.com>
2021-06-19 13:56:01 +0530

    Add files via upload

---
+++ Day01/Program05/BinOctHexConversion.py
@@ -0,0 +1,34 @@
+'''
+   5.	Program to Convert Decimal to Binary, Octal and Hexadecimal
+'''
+
+import sys
+
+def convert_to_Base(num,base):
+    '''
+       input : decimal number and base(2,8,16)
+       output: converted string
+    '''
+
+    output=""
+    while(num != 0):
+        rem = num % base
+        # if rem > 9 convert to 'A' ,'B', 'C', 'D', 'E', 'F' for  Hexadecimal
+        if(rem > 9):
+           output = chr(rem+55) + output
+        else:
+            output = str(rem) + output
+        num //= base
+    return output
+
+#main
+try:
+    num = int(sys.argv[1])
+    print("Binary: ", convert_to_Base(num, 2))  #Binary Conversion
+    print("Octal: ", convert_to_Base(num, 8))   #Octal Conversion
+    print("HexaDecimal: ",convert_to_Base(num,16))   #HexaDecimal Conversion
+except IndexError:
+    print("Enter a number as command line argument")
+except ValueError:
+    print("Enter a valid Decimal Number")
+
