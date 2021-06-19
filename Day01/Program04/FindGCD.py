10e10b6b5ff361d084d4fa159f56b9cb4a5e40c1
kavi3103 <48648214+kavi3103@users.noreply.github.com>
2021-06-19 13:54:40 +0530

    Add files via upload

---
+++ Day01/Program04/FindGCD.py
@@ -0,0 +1,25 @@
+'''
+    4.	Program to Find HCF or GCD
+'''
+import sys
+
+try:
+    #get input from command line
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+
+    #if first number is less than second, swap
+    if(num2 > num1):
+        num1,num2 = num2,num1
+
+    #Eucledian Algorithm to find GCD
+    while(num2 != 0):
+        temp = num1 % num2
+        num1 = num2
+        num2 = temp
+    print("GCD: ", num1)
+
+except ValueError:
+    print("Enter a valid integer")
+except IndexError:
+    print("Give two numbers")
